52224565ccce7d73046d0e9bb75b35168f7b53c1
alexmkinsman <akinsman825@gmail.com>
2022-09-14 16:29:02 -0700

    developing

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,7 @@
 const apiKey = "709eaff6487fadf0c26e0bc441b8c854"
 
 let searchForm = document.getElementById("searchForm");
+let searchInput = document.getElementById("searchInput");
 let searchButton = document.getElementById("searchButton");
 let pastSearch = document.getElementById("pastSearch");
 let currentWeather = document.getElementById("currentWeather");
@@ -9,26 +10,59 @@ let currentTemp = document.getElementById("currentTemp");
 let currentWind = document.getElementById("currentWind");
 let currentHum = document.getElementById("currentHum");
 let currentUV = document.getElementById("currentUV");
+let city;
+let lat, lon;
 
-searchButton.addEventListener('click', callAPI);
+searchButton.addEventListener('click', callWeather);
 
-function callAPI() {
-  var weatherURL = 'https://api.openweathermap.org/data/2.5/weather?lat=33.44&lon=-94.04&appid={apiKey}'
-  console.log(weatherURL);
-  fetch(weatherURL)
+function callWeather() {
+  //call city
+  //this will take the input and trims extra characters to find city in database
+  city = searchInput.value.trim()
+  console.log(city);
+  //fetch coordinates
+  var geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`
+  
+  fetch(geoURL)
   .then(function(response){
-    if (response) {
-      return response.json();
+    return response.json();
+  })
+  .then(function(data){
+    if (data) {
+      console.log("geo response = ", data);
+      // return data.json();
+      lat = data[0].lat;
+      lon = data[0].lon;
   } else {
       console.log(err);
   }
   })
-  .then (function (weather) {
+  .then (function(data) {
+    var weatherURL = `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`
+      //console.log(weatherURL);
+      
+      fetch(weatherURL)
+      .then(function(response){
+        if (response) {
+          console.log("weather response = ", response);
+          return response.json();
+      } else {
+          console.log(err);
+      }
+      })
+      .then (function(data) {
 
+      })
   })
 }
 
 let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
   console.log(searchHistory);
   
-  
+
+  //call API
+  //convert city to geolocation
+  //use lon and lat to find current weather
+  //assign current weather to html element
+  //display data in browser
+  //store searches in local storage

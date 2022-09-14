587e97ded0fee0731b2d4db5acc5147a60fcaa9f
alexmkinsman <akinsman825@gmail.com>
2022-09-14 15:46:14 -0700

    working on API

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,34 @@
-var requestURL = 'https://api.openweathermap.org/data/2.5/weather?lat=33.44&lon=-94.04&appid=709eaff6487fadf0c26e0bc441b8c854'
+const apiKey = "709eaff6487fadf0c26e0bc441b8c854"
 
-fetch(requestURL)
-.then(function(response){
-  return response.json();
-  })
-.then(function(data){
-  console.log(data);
-  })
-
+let searchForm = document.getElementById("searchForm");
+let searchButton = document.getElementById("searchButton");
+let pastSearch = document.getElementById("pastSearch");
 let currentWeather = document.getElementById("currentWeather");
 let currentDate = document.getElementById("currentDate");
+let currentTemp = document.getElementById("currentTemp");
+let currentWind = document.getElementById("currentWind");
+let currentHum = document.getElementById("currentHum");
+let currentUV = document.getElementById("currentUV");
+
+searchButton.addEventListener('click', callAPI);
+
+function callAPI() {
+  var weatherURL = 'https://api.openweathermap.org/data/2.5/weather?lat=33.44&lon=-94.04&appid={apiKey}'
+  console.log(weatherURL);
+  fetch(weatherURL)
+  .then(function(response){
+    if (response) {
+      return response.json();
+  } else {
+      console.log(err);
+  }
+  })
+  .then (function (weather) {
+
+  })
+}
+
+let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+  console.log(searchHistory);
+  
+  
